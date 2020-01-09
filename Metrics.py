d9db42a2bdbe8dadd8589d48c381b092d33167c4
CaydenEhrlich <37812412+CaydenEhrlich@users.noreply.github.com>
2020-01-08 20:35:23 -0600

    Added an Equality of Opportunity metric.

--- Metrics.py
+++ Metrics.py
@@ -1,4 +1,9 @@
+import pandas as pd
+from DataSet import DataSet
+
 class Metrics:
+
+	#TODO: Maybe make the DataSet be an instance variable?
 
 	def __init__(self):
 		pass
@@ -50,3 +55,45 @@ class Metrics:
 					realPosOrNeg +=1
 
 			return bothPosOrNeg/realPosOrNeg * 100
+
+	'''
+	Segments the overarching DataSet into smaller (non-overlapping) subsets by protected attribute. Returns a list of these
+	subsets as well as a list of the corresponding protected attribute values.
+		dataSet (DataSet) - the overarching DataSet NOTE WE'RE PROBABLY GETTING RID OF THIS PART WHEN WE MAKE IT AN INSTANCE VARIABLE
+	'''
+	def determineGroups(self, dataSet):
+		df = dataSet.dataFrame
+		possibleGroups = df[dataSet.protectedAttributes].unique()
+
+		organizedDataSetList = []
+		for value in possibleGroups:
+			# Setting up the group as a new DataSet
+			newDataSet = DataSet()
+			newDataSet.fileName = dataSet.fileName
+			newDataSet.dataFrame = df[df.protectedAttributes[0] == value]
+			newDataSet.protectedAttributes = dataSet.protectedAttributes
+			newDataSet.trueLabels = dataSet.trueLabels
+			newDataSet.headers = dataSet.headers
+			newDataSet.numAttributes = dataSet.numAttributes
+
+			organizedDataSetList.append(newDataSet)
+
+		return organizedDataSetList, possibleGroups
+
+	'''
+	Equality of Opportunity is the metric that stipulates that the true positive rate for each protected attribute should
+	be the same or similar within reason (NOTE WE WILL FIGURE THAT SHIT OUT LATER). Returns a dictionary of the true positive
+	rate for each protected attribute value (where the protected attribute value is the key and the true positive rate is the value).
+		dataSet (DataSet) - the overarching DataSet NOTE WE'RE PROBABLY GETTING RID OF THIS PART WHEN WE MAKE IT AN INSTANCE VARIABLE
+	'''
+	def runEquOfOpportunity(self, dataset):
+		groupDataSets, possibleGroups = self.determineGroups(dataset)
+		truePosByAttribute = {}
+		for i in range(len(groupDataSets)):
+			#TODO: for now we are hardcoding in a 1 since Equality of Opportunity requests true positive rate
+			truePosRate = self.truePosOrNeg(groupDataSets[i], 1)
+			truePosByAttribute[possibleGroups[i]] = truePosRate
+
+		return truePosByAttribute
+
+
