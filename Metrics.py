71a73cc93dbcf3252a02ada6875ca4ca132c5f63
CaydenEhrlich <37812412+CaydenEhrlich@users.noreply.github.com>
2020-01-16 15:31:30 -0600

    Testing and fixing bugs in Equality of Opportunity. Also added chi-square test.

--- Metrics.py
+++ Metrics.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from DataSet import DataSet
+from scipy import stats
 
 class Metrics:
 
@@ -12,7 +13,7 @@ class Metrics:
 	Calculates basic accuracy for a classified DataSet. Basic accuracy is defined as the number of classifications 
 	that match the known ground truth values.
 		dataSet (DataSet) - the classified DataSet to calculate accuracy for
-	Note: This function assumes that the column header for Bayes classifications is "bayesClassification"
+	Note: This function assumes that the column header for Bayes classifications is "Bayes Classification"
 	'''
 	def calculateAccuracy(self, dataSet):
 		numCorrect = 0
@@ -20,7 +21,7 @@ class Metrics:
 
 		for i in range(dataFrame.shape[0]):
 			groundTruth = dataFrame.at[i, dataSet.trueLabels]
-			bayesClassification = dataFrame.at[i, "bayesClassification"]
+			bayesClassification = dataFrame.at[i, "Bayes Classification"]
 
 			if groundTruth == bayesClassification:
 				numCorrect += 1
@@ -38,23 +39,30 @@ class Metrics:
 	'''
 	def truePosOrNeg(self, dataSet, truePosOrNeg):
 		dataFrame = dataSet.dataFrame
-		possibleClassifications = dataFrame["bayesClassification"].unique()
+		possibleClassifications = dataFrame["Bayes Classification"].unique()
 		if len(possibleClassifications) != 2:
 			return "Cannot calculate true positive or negative rate for nonbinary classifications"
 		else:
-			bothPosOrNeg = 0
-			realPosOrNeg = 0 #actual number of people with this classification in the DataSet
+			matchesLabel = 0
+			actualLabel = 0 #actual number of people with this classification in the DataSet
 
 			for i in range(dataFrame.shape[0]):
 				groundTruth = dataFrame.at[i, dataSet.trueLabels]
-				bayesClassification = dataFrame.at[i, "bayesClassification"]
+				bayesClassification = dataFrame.at[i, "Bayes Classification"]
 
 				if bayesClassification == truePosOrNeg and groundTruth == truePosOrNeg:
-					bothPosOrNeg +=1
+					matchesLabel +=1
 				if groundTruth == truePosOrNeg:
-					realPosOrNeg +=1
+					actualLabel +=1
 
-			return bothPosOrNeg/realPosOrNeg * 100
+			return matchesLabel, actualLabel
+
+
+	'''
+	Takes the information from truePosOrNeg and turns it into a rate
+	'''
+	def truePosOrNegRate(self, matchesLabel, actualLabel):
+		return matchesLabel / actualLabel * 100
 
 	'''
 	Segments the overarching DataSet into smaller (non-overlapping) subsets by protected attribute. Returns a list of these
@@ -63,22 +71,50 @@ class Metrics:
 	'''
 	def determineGroups(self, dataSet):
 		df = dataSet.dataFrame
-		possibleGroups = df[dataSet.protectedAttributes].unique()
+		possibleGroups = df[dataSet.protectedAttributes[0]].unique()
 
 		organizedDataSetList = []
 		for value in possibleGroups:
 			# Setting up the group as a new DataSet
 			newDataSet = DataSet()
 			newDataSet.fileName = dataSet.fileName
-			newDataSet.dataFrame = df[df.protectedAttributes[0] == value]
+			newDataSet.dataFrame = df[df[dataSet.protectedAttributes[0]] == value]
 			newDataSet.protectedAttributes = dataSet.protectedAttributes
 			newDataSet.trueLabels = dataSet.trueLabels
 			newDataSet.headers = dataSet.headers
 			newDataSet.numAttributes = dataSet.numAttributes
 
+			#resets indices for later indexing
+			newDataSet.dataFrame.reset_index(inplace=True, drop=True)
+
 			organizedDataSetList.append(newDataSet)
 
 		return organizedDataSetList, possibleGroups
+
+
+	#TODO: write this comment
+
+	def chiSquare(self, truePosByAttribute, trueNegByAttribute, TPTotal, TNTotal):
+		keys = truePosByAttribute.keys()
+		totalDict = {}
+		for item in keys:
+			posVal = truePosByAttribute[item]
+			negVal = trueNegByAttribute[item]
+			totalDict[item] = posVal + negVal
+
+		overallTotal = sum(totalDict.values())
+
+		chiSquare = 0
+		for item in keys:
+			expectedPosValue = (totalDict[item] * TPTotal) / overallTotal
+			posOutcome = ((truePosByAttribute[item] - expectedPosValue) ** 2) / truePosByAttribute[item]
+
+			expectedNegValue = (totalDict[item] * TNTotal) / overallTotal
+			negOutcome = ((trueNegByAttribute[item] - expectedNegValue) ** 2) / trueNegByAttribute[item]
+
+			chiSquare += posOutcome + negOutcome
+
+		return chiSquare
 
 	'''
 	Equality of Opportunity is the metric that stipulates that the true positive rate for each protected attribute should
@@ -86,14 +122,37 @@ class Metrics:
 	rate for each protected attribute value (where the protected attribute value is the key and the true positive rate is the value).
 		dataSet (DataSet) - the overarching DataSet NOTE WE'RE PROBABLY GETTING RID OF THIS PART WHEN WE MAKE IT AN INSTANCE VARIABLE
 	'''
+	#TODO: test this lol
 	def runEquOfOpportunity(self, dataset):
 		groupDataSets, possibleGroups = self.determineGroups(dataset)
 		truePosByAttribute = {}
+		TPTotal = 0
 		for i in range(len(groupDataSets)):
 			#TODO: for now we are hardcoding in a 1 since Equality of Opportunity requests true positive rate
-			truePosRate = self.truePosOrNeg(groupDataSets[i], 1)
-			truePosByAttribute[possibleGroups[i]] = truePosRate
+			truePosCount = self.truePosOrNeg(groupDataSets[i], 1)[0]
+			truePosByAttribute[possibleGroups[i]] = truePosCount
+			TPTotal += truePosCount
 
-		return truePosByAttribute
+		TNTotal = 0
+		trueNegByAttribute = {}
+		for i in range(len(groupDataSets)):
+			# TODO: for now we are hardcoding in a 0 since Equality of Opportunity requests true positive rate
+			trueNegCount = self.truePosOrNeg(groupDataSets[i], 0)[0]
+			trueNegByAttribute[possibleGroups[i]] = trueNegCount
+			TNTotal += trueNegCount
+
+		chiSquareVal = self.chiSquare(truePosByAttribute, trueNegByAttribute, TPTotal, TNTotal)
+
+		#to find degreeOfFreedom, multiply rows - 1 by columns - 1, num rows is always 2, thus this is just columns - 1
+		degreeOfFreedom = len(keys) - 1
+
+		#finds p value using the cumulative distribution function
+		pValue = 1 - stats.chi2.cdf(chiSquareVal, degreeOfFreedom)
+
+		EquOfOpp = False
+		if pValue < 0.05:
+			EquOfOpp = True
+
+		return pValue, EquOfOpp
 
 
