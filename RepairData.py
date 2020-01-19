9ea305c5809de5dbc6193bb08f04f457b414b483
CaydenEhrlich <37812412+CaydenEhrlich@users.noreply.github.com>
2020-01-19 13:00:22 -0600

    Implemented binary search for getBucket.

--- RepairData.py
+++ RepairData.py
@@ -49,6 +49,7 @@ class RepairData:
     Takes the list of distributions from makeDistributions and puts the values into buckets.
         distributions (list of lists) - the values from a single column separated by a protectedAttribute value
     '''
+    # TODO: MAKE SURE MINMAXLIST LOOKS OKAY
     def bucketize(self, distributions):
         # bucketAssignments is a list containing the index values for the bucket that the distribution values should end up in
         # e.g. [0, 1, 2, 3, 0] assigns the first and last items to bucket 0, the second item to bucket 2, etc.
@@ -64,7 +65,16 @@ class RepairData:
                 # Use the bucket assignment to append the distribution value to the appropriate bucket
                 bucketList[i][bucketAssignments[i][j]].append(distributions[i][j])
 
-        return bucketList
+        minMaxList = []
+        for i in range(len(bucketList)):
+            distributionList = []
+            for j in range(len(bucketList[i])):
+                minimum = min(bucketList[i][j])
+                maximum = max(bucketList[i][j])
+                distributionList.append([minimum, maximum])
+            minMaxList.append(distributionList)
+
+        return bucketList, minMaxList
 
     '''
     Takes in bucketized values and returns a median distribution.
@@ -93,14 +103,14 @@ class RepairData:
             attribute's values, organized by bucket
         attributeValues (list of strings) - a list of all possible values for the current protected attribute
     '''
-    def modifyData(self, columnName, medianDistribution, bucketList, attributeValues):
+    def modifyData(self, columnName, medianDistribution, bucketList, minMaxList, attributeValues):
         df = self.dataSetCopy.dataFrame
 
         for i in range(df.shape[0]):
             protectedAttributeValue = df.at[i, self.dataSetCopy.protectedAttribute]
             indexForProtectedAttributeValue = attributeValues.index(protectedAttributeValue)
             currentValue = df.at[i, columnName]
-            bucket = self.getBucket(currentValue, indexForProtectedAttributeValue, bucketList)
+            bucket = self.getBucket(currentValue, indexForProtectedAttributeValue, bucketList, minMaxList)
             df.loc[[i], [columnName]] = medianDistribution[bucket]
 
     '''
@@ -109,14 +119,34 @@ class RepairData:
         indexForProtectedAttributeValue (int) - the index within bucketList for a given protected attribute
         bucketList (list of list of list of floats) - a list of distributions of a protected 
             attribute's values, organized by bucket
+        minMaxList (list of list of list of floats) - a list of lists of the minimum and maximum in each bucket
     '''
-    def getBucket(self, value, indexForProtectedAttributeValue, bucketList):
+    def getBucket(self, value, indexForProtectedAttributeValue, bucketList, minMaxList):
         bucketedDistribution = bucketList[indexForProtectedAttributeValue]
-        #TODO: Note: this will be bad for big data sets
-        #TODO: Binary search maybe???
-        for bucket in bucketedDistribution:
-            if value in bucket:
-                return bucketedDistribution.index(bucket)
+        minMaxSublist = minMaxList[indexForProtectedAttributeValue]
+
+        return self.getBucketHelper(value, 0, len(bucketedDistribution) - 1, bucketedDistribution, minMaxSublist)
+
+    '''
+    Helper function for getBucket binary search.
+        value (float) - the value to find
+        start (int) - the index of where to start searching
+        stop (int) - the index of where to stop searching
+        bucketedDistribution (list of list of floats) - a distribution of a protected attribute value, organized by bucket
+        minMaxSublist (list of list of floats) - a list of the minimum and maximum in each bucket
+    '''
+
+    def getBucketHelper(self, value, start, stop, bucketedDistribution, minMaxSublist):
+        middleIndex = (start + stop) // 2
+
+        if value > minMaxSublist[middleIndex][1]:
+            return self.getBucketHelper(value, middleIndex + 1, stop, bucketedDistribution, minMaxSublist)
+
+        elif value < minMaxSublist[middleIndex][0]:
+            return self.getBucketHelper(value, start, middleIndex, bucketedDistribution, minMaxSublist)
+
+        else:
+            return middleIndex
 
     '''
     Creates a DataSet object
@@ -139,9 +169,9 @@ class RepairData:
     '''
     def repairColumn(self, columnName):
         distributions, attributeValues = self.makeDistributions(self.dataSetCopy.protectedAttribute, columnName)
-        bucketList = self.bucketize(distributions)
+        bucketList, minMaxList = self.bucketize(distributions)
         medianDistributions = self.findMedianDistribution(bucketList)
-        self.modifyData(columnName, medianDistributions, bucketList, attributeValues)
+        self.modifyData(columnName, medianDistributions, bucketList, minMaxList, attributeValues)
 
     '''
     Makes DataSet object from a file, then repairs the data
