f9d8d4fdbf106127703c846d88d87e8766e30868
Irene Sakson <saksoni@carleton.edu>
2020-01-26 20:56:02 -0600

    Changed DI algorithm so it no longer splits data into test/train sets. Also cleaned up and commented code.

--- classifierForDI.py
+++ classifierForDI.py
@@ -1,68 +1,54 @@
 from DataSet import DataSet
 from sklearn.svm import SVC
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import balanced_accuracy_score, confusion_matrix
 
-
 '''
-Splits data into training and test sets by shuffling the order of the rows and splitting into 
-two DataFrames according to the ratios provided.
-    dataFrame (DataFrame) - the dataFrame to split into training and test sets
-    fractionTrain (float between 0 and 1) - the percentage of the data that should be in the training set
-    fractionTest (float between 0 and 1) - the percentage of the data that should be in the training set
-        Note: fractionTrain and fractionTest should sum to 1.
-    classifierCol (string) - the header for the column that the classifier should be trying to classify
+Runs an sklearn SVC SVM classifier on the specified data.
+    data (DataFrame) - The DataFrame object whose data the classifier should use
+    classifications (column of DataFrame) - The true classifications for the data
 '''
-#TODO: write return for comment above
-
-def splitDataIntoTrainTest(dataFrame, fractionTrain, fractionTest, classifierCol):
-    train_test = train_test_split(dataFrame, train_size=fractionTrain, test_size=fractionTest, shuffle=True)
-
-    trainClassifications = train_test[0][classifierCol]
-    testClassifications = train_test[1][classifierCol]
-
-    return train_test, trainClassifications, testClassifications
-
-'''
-Runs an SVC SVM classifier on the specified training and test sets.
-'''
-def classify(train_test, trainClassifications, testClassifications):
+def classify(data, classifications):
     classifier = SVC(class_weight='balanced')
-    classifier.fit(train_test[0], trainClassifications)
-    return classifier.predict(train_test[1])
+    classifier.fit(data, classifications)
+    return classifier.predict(data)
 
 '''
 Computes the balanced error rate (BER) for the classifier results.
+    data (DataFrame) - The DataFrame object to compute BER for
+    classifierCol (string) - The column header for the true classifications (protected attribute) in the data
+    classifierResults (array) - An array of classifications (guessed by the classifier)
 '''
-def computeBER(train_test, classifierCol, classifierResults):
-    bsr = balanced_accuracy_score(train_test[1][classifierCol], classifierResults)
+def computeBER(data, classifierCol, classifierResults):
+    bsr = balanced_accuracy_score(data[classifierCol], classifierResults)
     ber = 1 - bsr
     return ber
 
 '''
 Computes the parameter Beta for the algorithm.
+    data (DataFrame) - The DataFrame object to compute BER for
+    classifierCol (string) - The column header for the true classifications (protected attribute) in the data
+    classifierResults (array) - An array of classifications (guessed by the classifier)
 '''
-def computeBeta(train_test, classifierCol, classifierResults):
-    confusionMatrix = confusion_matrix(train_test[1][classifierCol], classifierResults)
+def computeBeta(data, classifierCol, classifierResults):
+    confusionMatrix = confusion_matrix(data[classifierCol], classifierResults)
     beta = confusionMatrix[0, 1] / (confusionMatrix[0, 0] + confusionMatrix[0, 1])
     return beta
 
+'''
+Runs the full Feldman disparate impact detection algorithm. 
+    dataSet (DataSet) - The DataSet object to retrieve the data from
+'''
 def detectDI(dataSet):
     copy = dataSet.copyDataSet()
     copy.makeNumerical(copy.protectedAttributes[0]) #TODO: remove [0] once it's pushed onto master
-    dummified = copy.dummify()
+    dummifiedData = copy.dummify()
 
     classifierCol = copy.protectedAttributes[0] #TODO also change
-    data_split = splitDataIntoTrainTest(dummified, .8, .2, classifierCol)
-    classifications = classify(data_split[0], data_split[1], data_split[2])
-    ber = computeBER(data_split[0], classifierCol, classifications)
-    beta = computeBeta(data_split[0], classifierCol, classifications)
+    classifications = classify(dummifiedData, dummifiedData[classifierCol])
+    ber = computeBER(dummifiedData, classifierCol, classifications)
+    beta = computeBeta(dummifiedData, classifierCol, classifications)
     epsilonPrime = 1 / 2 - beta / 8  # allowable BER threshold
     if epsilonPrime < ber:
         return "No disparate impact."
     else:
         return "Possible disparate impact."
-
-ds = DataSet()
-ds.loadData("ClassifiedRicciData.csv", ["Race"], "Class")
-print(detectDI(ds))
